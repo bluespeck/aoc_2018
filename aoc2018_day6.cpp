19c19951f3d8c60052452759e81988a2ae6c8296
Mihai Tudorache <mstudorache@gmail.com>
2018-12-07 06:59:20 +0200

    refactored code a bit

--- aoc2018_day6.cpp
+++ aoc2018_day6.cpp
@@ -1,15 +1,11 @@
 #include <algorithm>
 #include <array>
+#include <cassert>
 #include <cstdio>
 #include <iostream>
 #include <map>
 #include <string>
 #include <vector>
-
-int md(int x1, int y1, int x2, int y2)
-{
-    return x2 - x2 + y2 - y1;
-}
 
 struct Point
 {
@@ -17,48 +13,24 @@ struct Point
     int y = 0;
 };
 
-using PointVec = std::vector<Point>;
+struct Rectangle
+{
+    Point p1, p2;
+};
 
-int md(Point p1, Point p2)
+using PointVec = std::vector<Point>;
+using ProximityGrid = std::vector<std::vector<int>>;
+
+int ManhattanDistance(Point p1, Point p2)
 {
     return std::abs(p1.x - p2.x) + std::abs(p1.y - p2.y);
 }
 
-void remove(int** grid, int m, int n, int value)
+PointVec ReadInput()
 {
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (grid[i][j] == value)
-            {
-                grid[i][j] = -1;
-            }
-        }
-    }
-}
+    PointVec points;
 
-int count(int** grid, int m, int n, int value)
-{
-    int counter = 0;
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (grid[i][j] == value)
-            {
-                counter++;
-            }
-        }
-    }
-    return counter;
-}
-
-int main()
-{
-    std::vector<Point> points;
-    Point ul{ -1,-1 }, lr{ -1,-1 };
-    while(true)
+    while (true)
     {
         char c;
         Point p;
@@ -66,44 +38,94 @@ int main()
             break;
         std::cin >> c >> p.y;
         points.push_back(p);
-
-        if (ul.x > p.x || ul.x == -1)
-            ul.x = p.x;
-        if (ul.y > p.y || ul.y == -1)
-            ul.y = p.y;
-        if (lr.x < p.x || lr.x == -1)
-            lr.x = p.x;
-        if (lr.y < p.y || lr.y == -1)
-            lr.y = p.y;
     }
 
-    int m = lr.x - ul.x + 1;
-    int n = lr.y - ul.y + 1;
-    int** grid = new int*[m];
+    return points;
+}
+
+Rectangle FindGridLimits(PointVec points)
+{
+    Point upperLeft{ -1,-1 };
+    Point lowerRight{ -1,-1 };
+    for(auto& p : points)
+    {
+        if (upperLeft.x > p.x || upperLeft.x == -1)
+            upperLeft.x = p.x;
+        if (upperLeft.y > p.y || upperLeft.y == -1)
+            upperLeft.y = p.y;
+        if (lowerRight.x < p.x || lowerRight.x == -1)
+            lowerRight.x = p.x;
+        if (lowerRight.y < p.y || lowerRight.y == -1)
+            lowerRight.y = p.y;
+    }
+
+    return { upperLeft, lowerRight };
+}
+
+// Marks a grid area with -1 (practically excluding a pointId from further being considered)
+void ExcludeArea(ProximityGrid& grid, int pointId)
+{
+    for (int i = 0; i < grid.size(); i++)
+    {
+        for (int j = 0; j < grid[i].size(); j++)
+        {
+            if (grid[i][j] == pointId)
+            {
+                grid[i][j] = -1;
+            }
+        }
+    }
+}
+
+size_t Count(ProximityGrid& grid, int value)
+{
+    size_t counter = 0;
+
+    for (auto& gridRow : grid)
+    {
+        counter += std::count(gridRow.begin(), gridRow.end(), value);
+    }
+
+    return counter;
+}
+
+size_t LargestNotInfiniteProximityArea(PointVec& points, const Rectangle& proximityGridLimits)
+{
+    auto[upperLeft, lowerRight] = proximityGridLimits;
+
+    int m = lowerRight.x - upperLeft.x + 1;
+    int n = lowerRight.y - upperLeft.y + 1;
+
+    std::vector<std::vector<int>> grid(m);
     for (int i = 0; i < m; i++)
     {
-        grid[i] = new int[n];
+        grid[i].resize(n);
     }
 
-    for (int x = ul.x; x <= lr.x; x++)
+    for (int x = upperLeft.x; x <= lowerRight.x; x++)
     {
-        for (int y = ul.y; y <= lr.y; y++)
+        for (int y = upperLeft.y; y <= lowerRight.y; y++)
         {
             Point p{ x, y };
             std::vector<Point> mds;
             for (int i = 0; i < points.size(); i++)
             {
-                mds.push_back(Point{ md(p, points[i]), i });
+                mds.push_back(Point{ ManhattanDistance(p, points[i]), i });
             }
             std::sort(mds.begin(), mds.end(), [](Point a, Point b) {return a.x < b.x; });
 
+            int gridX = x - upperLeft.x;
+            int gridY = y - upperLeft.y;
+            assert(gridX >= 0 && gridX < grid.size());
+            assert(gridY >= 0 && gridY < grid[gridX].size());
+
             if (mds.size() > 1 && mds[0].x == mds[1].x)
             {
-                grid[x - ul.x][y - ul.y] = -1;
+                grid[gridX][gridY] = -1;
             }
             else
             {
-                grid[x - ul.x][y - ul.y] = mds[0].y;
+                grid[gridX][gridY] = mds[0].y;
             }
         }
     }
@@ -112,45 +134,52 @@ int main()
     {
         if (grid[i][0] != -1)
         {
-            remove(grid, m, n, grid[i][0]);
+            ExcludeArea(grid, grid[i][0]);
         }
         if (grid[i][n - 1] != -1)
         {
-            remove(grid, m, n, grid[i][n - 1]);
+            ExcludeArea(grid, grid[i][n - 1]);
         }
     }
     for (int j = 0; j < n; j++)
     {
         if (grid[0][j] != -1)
         {
-            remove(grid, m, n, grid[0][j]);
+            ExcludeArea(grid, grid[0][j]);
         }
-        if (grid[m-1][j] != -1)
+        if (grid[m - 1][j] != -1)
         {
-            remove(grid, m, n, grid[m-1][j]);
+            ExcludeArea(grid, grid[m - 1][j]);
         }
 
     }
 
-    std::vector<int> areas;
-    for (int i = 0; i < points.size(); i++)
+    std::vector<size_t> areas;
+    for (size_t i = 0; i < points.size(); i++)
     {
-        areas.push_back(count(grid, m, n, i));
+        areas.push_back(Count(grid, static_cast<int>(i)));
     }
 
-    std::cout << *std::max_element(areas.begin(), areas.end()) << '\n';
+    return *std::max_element(areas.begin(), areas.end());
+}
+
+size_t LargestAreaWithBoundTotalCellDistanceToAllPoints(const PointVec& points, const Rectangle& proximityGridLimits, int distanceBound)
+{
+    auto[upperLeft, lowerRight] = proximityGridLimits;
+
     int area = 0;
-    for (int x = ul.x; x <= lr.x; x++)
+
+    for (int x = upperLeft.x; x <= lowerRight.x; x++)
     {
-        for (int y = ul.y; y <= lr.y; y++)
+        for (int y = upperLeft.y; y <= lowerRight.y; y++)
         {
             Point p{ x, y };
             int sum = 0;
             bool invalid = false;
             for (int i = 0; i < points.size(); i++)
             {
-                sum += md(p, points[i]);
-                if (sum >= 10000)
+                sum += ManhattanDistance(p, points[i]);
+                if (sum >= distanceBound)
                 {
                     invalid = true;
                     continue;
@@ -162,7 +191,18 @@ int main()
         }
     }
 
+    return area;
+}
 
-    std::cout << area << '\n';
+int main()
+{
+    PointVec points = ReadInput();
+
+    Rectangle proximityGridLimits = FindGridLimits(points);
+
+    std::cout << LargestNotInfiniteProximityArea(points, proximityGridLimits) << '\n';
+
+    std::cout << LargestAreaWithBoundTotalCellDistanceToAllPoints(points, proximityGridLimits, 10000) << '\n';
+
     return 0;
 }
